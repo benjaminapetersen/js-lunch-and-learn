5e5d7c4a25678997a1e843213bac43bbce0d2222
benjaminapetersen <admin@benjaminapetersen.me>
2016-06-16 13:03:44 -0400

    Finish out simple edit/delete function using naive jQuery based approach for local-storage exercise

--- exercises/local-storage/src/scripts/app.js
+++ exercises/local-storage/src/scripts/app.js
@@ -1,6 +1,8 @@
-(function() {
+$(function() {
   'use strict';
 
+  // create "library" of functions to use
+  // -------------------------------------------------------------- */
   // data is an array of objects that look like this:
   // [
   //   {name: '', value: ''},
@@ -21,16 +23,100 @@
     return person;
   };
 
+  var prepForForm = function(obj) {
+    var keys = Object.keys(obj);
+    return keys.map(function(key) {
+      return {
+        name: key,
+        value: obj[key]
+      };
+    });
+  };
 
-  // this will be our list of people
-  var people = [];
+  var setForm = function(vals) {
+    vals.forEach(function(item) {
+      $('#'+item.name, $form).val(item.value);
+    });
+  };
 
+
+  var render = function(list, $tpl, $container) {
+    $container.empty();
+    var toAppend = '';
+    list.forEach(function(item, i) {
+      // This is a fairly naive way to do this, there are much better
+      // templating functions, such as _.template()
+      var html = $tpl.text();
+      html = html.replace('{firstName}', item.firstName);
+      html = html.replace('{lastName}', item.lastName);
+      html = html.replace('{age}', item.age);
+      html = html.replace('{email}', item.email);
+      html = html.replace(/\{index\}/g, i); // need to use a regex w /g for multple...
+      toAppend = toAppend + html;
+    });
+    $container.append(toAppend);
+  };
+
+  // note: every time we makeEditable() we generate new jQuery objects
+  // and just ignore old. Newer frameworks will provide better ways to
+  // optimize this!
+  var makeEditable = function(list, $container) {
+
+    $('.edit', $container).click(function(e) {
+      e.preventDefault();
+      var index = $(this).data('index');
+      var person = list.splice(index, 1)[0];
+      // take the person out of the flow
+      // $personTpl here is shady :/
+      render(list, $personTpl, $container);
+      var forForm = prepForForm(person);
+      setForm(forForm);
+    });
+
+    $('.delete', $container).click(function(e) {
+      e.preventDefault();
+      var index = $(this).data('index');
+      list.splice(index, 1);
+      // $personTpl here is shady :/
+      render(list, $personTpl, $container);
+      // make permanent
+      localStorage.setItem(storageKey, JSON.stringify(list));
+    });
+
+  };
+
+
+  // setup localStorage
+  // -------------------------------------------------------------- */
   // pick a key to save things to in localStorage
   var storageKey = 'people';
+  // this will be our list of people
+  var people = JSON.parse(localStorage.getItem(storageKey)) || [];
 
 
-  // get the form with jQuery so we can do stuff with it
+  // get the jQuery objects/DOM nodes we need
+  // -------------------------------------------------------------- */
   var $form = $('form[name="person"]');
+  var $clear = $('#clear', $form);
+  var $firstInput = $('#firstName', $form);
+  var $personTpl = $('#personTpl'); // .html() more efficient?
+  var $peopleContainer = $('#peopleContainer');
+
+
+  // render once
+  // -------------------------------------------------------------- */
+  render(people, $personTpl, $peopleContainer);
+  makeEditable(people, $peopleContainer);
+
+
+
+  // setup the form
+  // -------------------------------------------------------------- */
+  $clear.on('click', function() {
+    $form[0].reset();
+    $firstInput.focus();
+  });
+
 
   // do stuff when the form submits
   $form.submit(function(evt) {
@@ -40,22 +126,27 @@
     // now get the form data, BUT, will only get
     // inputs with a name="" attribute
     var data = $form.serializeArray();
-    console.log('form data', data);
+
     // transform.
     var person = makePerson(data);
-    console.log('make person', person);
 
     // add this person to our list of people.
     people.push(person);
+
+    render(people, $personTpl, $peopleContainer);
+    makeEditable(people, $peopleContainer);
 
     // prep all people to save, otherwise it will put
     // [object Object] into localStorage, and that means
     // all of our data is lost :(
     var toSave = JSON.stringify(people);
-    console.log('stringify to save', toSave);
 
     // now save them!
     localStorage.setItem(storageKey, toSave);
+
+    // does jQuery provide or do we need the underlying form node?
+    this.reset();
+    $firstInput.focus();
   });
 
-})();
+});
