b9c1a98c71a62aba3eeac48f767a31a08dac45b1
benjaminapetersen <admin@benjaminapetersen.me>
2016-09-28 13:48:33 -0400

    Initial todo app page, views/index.html

--- projects/todo-app/app.js
+++ projects/todo-app/app.js
@@ -14,7 +14,8 @@ var api_todos = require('./routes/api/todos');
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+// app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
