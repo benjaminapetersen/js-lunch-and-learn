69afb5f693160f0a8df6b457fa8a4f5634ca3c4f
Amineta Lo <amilo@redhat.com>
2018-06-22 11:08:09 -0400

    06-21-18 - getting user input value, setting state onChange

--- projects/react-todo-app/src/components/todo-item/TodoItem.js
+++ projects/react-todo-app/src/components/todo-item/TodoItem.js
@@ -3,7 +3,10 @@ import React from 'react';
 const todoItems = props => {
   const items = props.items.map((item, i) => <li className="todo__item" key={i}>
       <div className="input-group input-group-lg">
-        <input type="text" className={`todo__item__toggle form-control ${item.complete && 'todo__item__toggle--completed'}`} value={item.text} />
+        <input 
+        type="text" 
+        className={`todo__item__toggle form-control ${item.complete && 'todo__item__toggle--completed'}`} 
+        value={item.text} />
         <span className="input-group-btn">
           <button className={`todo__item__star btn btn-default ${item.starred && 'todo__item__star--active'}`}><i className="fa fa-star"></i></button>
           <button className="todo__item__remove btn btn-default"><i className="fa fa-times"></i></button>
